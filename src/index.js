1f317263046a458c893d6c5463ddfd304955ba0e
Ania <anna.kujawa@chimeraprime.com>
2021-02-13 21:10:56 +0100

    add technologies and tools to career step

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './styles/main.scss';
 import welcome from './styles/assets/welcome.mp4';
 import "./styles/assets/background.png";
+import "./styles/assets/mongoDB_logo.png";
+import "./styles/assets/neo4j_logo.png";
+import "./styles/assets/redux-logo.png";
+import "./styles/assets/ts-logo.png";
+import "./styles/assets/nodejs_logo.png";
+import "./styles/assets/js_logo.png";
+import "./styles/assets/react_logo.png";
+import "./styles/assets/sass_logo.png";
+import "./styles/assets/jira_logo.svg";
+import "./styles/assets/trello_logo.svg";
+
 
 import './js/welcome';
 import './js/road';
--- src/js/road.js
+++ src/js/road.js
@@ -14,3 +14,12 @@ const observer = new IntersectionObserver(displayStep, options);
 
 const careerSteps = document.querySelectorAll('.skills-road__step');
 careerSteps.forEach(step => observer.observe(step));
+
+const expanders = document.querySelectorAll('.skills-road__step-arrow');
+
+expanders.forEach(expander => {
+  expander.addEventListener('click', () => {
+    const modal = document.querySelector('.skills-road__step-modal');
+    modal.classList.toggle('isHidden');
+  });
+})
