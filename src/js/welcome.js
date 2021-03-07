870d89a8149d4f783eeff589504d67114f3d25ee
Ania <anna.kujawa@chimeraprime.com>
2021-03-07 14:10:57 +0100

    add second color, improve responsivness, mobile menu

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,5 +1,25 @@
-window.setTimeout(() => {
-  Array.from(document.querySelectorAll('.delayed')).forEach(delayed => {
-    delayed.style.visibility = 'visible'
+const desktopBreakpoint = 1024;
+
+const addDesktopEvents = () => {
+  window.setTimeout(() => {
+    Array.from(document.querySelectorAll('.delayed')).forEach(delayed => {
+      delayed.style.visibility = 'visible'
+    });
+  }, 14000);
+};
+
+const addMobileEvents = () => {
+  const mobileMenuButton = document.querySelector('.vertical-menu');
+  const menu = document.querySelector('.menu');
+  
+  mobileMenuButton && mobileMenuButton.addEventListener("click", () => {
+    menu.classList.toggle("mobile-hide");
   });
-}, 14000);
+};
+
+if(window.screen.width < desktopBreakpoint) {
+  addMobileEvents();
+} else {
+  addDesktopEvents();
+}
+
